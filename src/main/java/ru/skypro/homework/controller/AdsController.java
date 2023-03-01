0ff290f5b203f5d710a16e34ee142b3ba9fb214c
evgen <89818121788@mail.ru>
2023-03-01 17:15:57 +0300

    Добавлен AdsController, CreateAds, ResponseWrapperAds, ResponseWrapperAdsComment

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.skypro.homework.dto.*;
 import ru.skypro.homework.service.AdsService;
 
 
@@ -18,64 +19,68 @@ public class AdsController {
 
 
     @GetMapping
-    public ResponseEntity getAllAds() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ResponseWrapperAds> getAllAds() {
+        return ResponseEntity.ok(new ResponseWrapperAds());
     }
 
     @PostMapping
-    public ResponseEntity addAds(@RequestBody String example) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<CreateAds> addAds(@RequestBody CreateAds createAds) {
+        return ResponseEntity.ok(createAds);
     }
 
     @GetMapping("/me")
-    public ResponseEntity getAdsMe() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ResponseWrapperAds> getAdsMe(@RequestParam(value = "authenticated", required = false) boolean authenticated,
+                                                       @RequestParam(value = "authorities[0].authority", required = false) String authorities0authority,
+                                                       @RequestParam(value = "credentials", required = false) Object credentials,
+                                                       @RequestParam(value = "details", required = false) Object details,
+                                                       @RequestParam(value = "principal", required = false) Object principal) {
+        return ResponseEntity.ok(new ResponseWrapperAds());
     }
 
     @GetMapping("/{ad_pk}/comment")
-    public ResponseEntity getAdsComment(@PathVariable Long ad_pk) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<ResponseWrapperAdsComment> getAdsComment(@PathVariable("ad_pk") String adPk) {
+        return ResponseEntity.ok(new ResponseWrapperAdsComment());
     }
 
     @PostMapping("/{ad_pk}/comment")
-    public ResponseEntity addAdsComments(@PathVariable Long ad_pk,
-                                         @RequestBody String example) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<AdsComment> addAdsComments(@PathVariable("ad_pk") String adPk,
+                                                     @RequestBody AdsComment adsComment) {
+        return ResponseEntity.ok(adsComment);
     }
 
     @DeleteMapping("/{ad_pk}/comment/{id}")
-    public ResponseEntity deleteAdsComment(@PathVariable Long ad_pk,
-                                           @PathVariable Long id){
+    public ResponseEntity deleteAdsComment(@PathVariable("ad_pk") String adPk,
+                                           @PathVariable Integer id) {
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{ad_pk}/comment/{id}")
-    public ResponseEntity getAdsComment(@PathVariable Long ad_pk,
-                                        @PathVariable Long id){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<AdsComment> getAdsComment(@PathVariable("ad_pk") String adPk,
+                                                    @PathVariable Integer id) {
+        return ResponseEntity.ok(new AdsComment());
     }
 
     @PatchMapping("/{ad_pk}/comment/{id}")
-    public ResponseEntity updateAdsComment(@PathVariable Long ad_pk,
-                                           @PathVariable Long id,
-                                           @RequestBody String example){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<AdsComment> updateAdsComment(@PathVariable("ad_pk") String adPk,
+                                                       @PathVariable Integer id,
+                                                       @RequestBody AdsComment adsComment) {
+        return ResponseEntity.ok(adsComment);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity removeAds(@PathVariable Long id) {
+    public ResponseEntity removeAds(@PathVariable Integer id) {
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity getAds(@PathVariable Long id){
-        return ResponseEntity.ok().build();
+    public ResponseEntity<FullAds> getAds(@PathVariable Integer id) {
+        return ResponseEntity.ok(new FullAds());
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity updateAds(@PathVariable Long id,
-                                    @RequestBody String example) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<Ads> updateAds(@PathVariable Integer id,
+                                         @RequestBody Ads ads) {
+        return ResponseEntity.ok(ads);
     }
 
 }
